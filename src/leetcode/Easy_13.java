04fdc0f27556683a1b536258f3206f57612f41b7
kimmin-ko <kimmin2301@gmail.com>
2021-03-03 13:21:39 +0900

    leetcode easy 13

--- src/leetcode/Easy_13.java
+++ src/leetcode/Easy_13.java
@@ -6,25 +6,45 @@ import java.util.Map;
 public class Easy_13 {
 
     public static void main(String[] args) {
-        String s = "XXVII";
+        String s = "D"; // 1695
         int result = romanToInt(s);
 
         System.out.println("result = " + result);
     }
 
     public static int romanToInt(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("V", 5);
-        map.put("X", 10);
-        map.put("L", 50);
-        map.put("C", 100);
-        map.put("D", 500);
-        map.put("M", 1000);
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        if(s.length() == 0)
+            return 0;
+        else if(s.length() == 1)
+            return map.get(s.charAt(0));
+
+        int result = 0;
+        for (int i = 1; i < s.length(); i++) {
+            int value = map.get(s.charAt(i - 1));
+            int nextVal = map.get(s.charAt(i));
+
+            if (nextVal > value) {
+                value = nextVal - value;
+                i++;
+            }
+
+            result += value;
+
+            if(i == s.length() - 1)
+                result += map.get(s.charAt(i));
+        }
 
 
-
-        return 0;
+        return result;
     }
 
 }
