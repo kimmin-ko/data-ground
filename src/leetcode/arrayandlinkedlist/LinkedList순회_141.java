301201b48572af13f6d4f74b485ba2c3fa71264f
Min Kim <=>
2021-04-04 12:49:27 +0900

    LinkedList 순회 완료

--- src/leetcode/arrayandlinkedlist/LinkedList순회_141.java
+++ src/leetcode/arrayandlinkedlist/LinkedList순회_141.java
@@ -1,13 +1,31 @@
 package leetcode.arrayandlinkedlist;
 
+import java.util.ArrayList;
+
 public class LinkedList순회_141 {
 
     public static void main(String[] args) {
-
+        ListNode d3 = new ListNode(4);
+        ListNode d2 = new ListNode(2, d3);
+        ListNode d1 = new ListNode(0, d2);
+        ListNode head = new ListNode(3, d1);
+        boolean result = hasCycle(head);
+        System.out.println("result = " + result);
     }
 
     public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
 
         return false;
     }
